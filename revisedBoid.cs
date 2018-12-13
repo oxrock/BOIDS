b8386acdf0fdab31932d1ffcb135c9470c8714b3
oxrock <oxrock2001@yahoo.com>
2018-12-13 17:51:45 -0500

    Add files via upload

---
+++ revisedBoid.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class revisedBoid : MonoBehaviour
+{
+    public Vector3 position;
+    public int fieldSize = 25;
+    public float flightSpeed = .5f;
+    public float alignDistance = 10f;
+    public float cohesionDistance = 10f;
+    public float avoidanceDistance = 10f;
+    public Vector3 momentum = new Vector3(0, 0, 0);
+    Vector3 alignForce = new Vector3(0, 0, 0);
+    Vector3 cohesionForce = new Vector3(0, 0, 0);
+    Vector3 seperationForce = new Vector3(0, 0, 0);
+    Vector3 BD_force = new Vector3(0, 0, 0);
+    Vector3 boundryTarget = new Vector3(0, 0, 0);
+    public float momMagnitude;
+    public float settingMagnitudes;
+    float turnSpeed = 5;
+    Vector3 normalized;
+
+
+    void seperation() {
+        int count = 0;
+        seperationForce = new Vector3(0, 0, 0);
+        foreach (revisedBoid b in singleton.instance.boids)
+        {
+            if (b != this)
+            {
+                float distance = Vector3.Distance(transform.position, b.transform.position);
+                if (distance < avoidanceDistance)
+                {
+                    Vector3 force = (transform.position - b.transform.position);
+                    seperationForce += force;
+                    count++;
+                }
+            }
+        };
+        seperationForce = Vector3.Normalize(seperationForce) * settingMagnitudes;
+    }
+
+    void align()
+    {
+        int count = 0;
+        alignForce = new Vector3(0, 0, 0);
+        foreach (revisedBoid b in singleton.instance.boids)
+        {
+            if (b != this)
+            {
+                if (Vector3.Distance(transform.position, b.transform.position) < alignDistance)
+                {
+                    alignForce += b.momentum;
+                    //alignForce += b.transform.rotation.eulerAngles;
+                    //this should probably be chance to reference their momentum, for now it can possibly be estimated to equal their rotation however
+                    count++;
+                }
+            }
+
+        }
+        //alignForce = zeroCheck(alignForce);
+        if (count > 0)
+        {
+            alignForce /= count;
+        }
+        alignForce = Vector3.Normalize(alignForce)*settingMagnitudes;
+        
+    }
+
+   
+    void randomRotate()
+    {
+        transform.Rotate(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+    }
+
+    void boundryAvoidance()
+    {
+        float distance = Vector3.Distance(boundryTarget, transform.position);
+        if (distance > fieldSize * .9)
+        {
+            BD_force = Vector3.Normalize(boundryTarget - transform.position)*5;
+        }
+
+    }
+
+    void cohesion()
+    {
+        cohesionForce = new Vector3(0, 0, 0);
+        Vector3 avgPosition = new Vector3();
+        int total = 0;
+        foreach (revisedBoid b in singleton.instance.boids)
+        {
+            if (b != this)
+            {
+                if (Vector3.Distance(transform.position, b.transform.position) < cohesionDistance)
+                {
+                    avgPosition += b.transform.position;
+                }
+            }
+        }
+        if (total > 1)
+        {
+            avgPosition /= total;
+        }
+        cohesionForce = Vector3.Normalize(avgPosition - transform.position)*settingMagnitudes;
+
+
+    }
+
+    
+
+
+    void Start()
+    {
+        singleton.instance.boids.Add(this);
+        fieldSize = singleton.instance.fieldSize;
+        position = new Vector3(Random.Range(-fieldSize*2, fieldSize * 2), Random.Range(-fieldSize, fieldSize), Random.Range(-fieldSize, fieldSize));
+        transform.position = position;
+        randomRotate();
+        //boundryTarget = new Vector3(0, 0, 0);
+        momentum = transform.forward;
+        alignDistance = singleton.instance.alignDistance;
+        cohesionDistance = singleton.instance.cohesionDistance;
+        avoidanceDistance = singleton.instance.alignDistance;
+        momMagnitude = singleton.instance.momMag;
+        settingMagnitudes = singleton.instance.setMag;
+        //flightSpeed = Random.Range(2f,4f);
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //enforceBoundries();
+        Vector3 changes = new Vector3(0,0,0);
+        align();
+        cohesion();
+        boundryAvoidance();
+        seperation();
+        momentum += seperationForce;
+        momentum += alignForce;
+        momentum += BD_force;
+        momentum += cohesionForce;
+        normalized = momentum.normalized;
+        momentum = normalized*momMagnitude;
+        if (momentum != Vector3.zero)
+        {
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(momentum, Vector3.up), 1);
+            transform.rotation = Quaternion.LookRotation(normalized, Vector3.up);
+            //print(momentum);
+        }
+        else {
+            //print("zero");
+        }
+
+
+        
+
+        transform.position += normalized * (flightSpeed * Time.fixedDeltaTime);
+
+    }
+
+
+
+    void enforceBoundries()
+    {
+        if (transform.position.x > fieldSize)
+        {
+            transform.position = new Vector3(-fieldSize, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -fieldSize)
+        {
+            transform.position = new Vector3(fieldSize, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.y > fieldSize)
+        {
+            transform.position = new Vector3(transform.position.x, -fieldSize, transform.position.z);
+        }
+        else if (transform.position.y < -fieldSize)
+        {
+            transform.position = new Vector3(transform.position.x, fieldSize, transform.position.z);
+        }
+
+        if (transform.position.z > fieldSize)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -fieldSize);
+        }
+        else if (transform.position.z < -fieldSize)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, fieldSize);
+        }
+    }
+}
---
+++ singleton.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public  class singleton : MonoBehaviour {
+
+    public static singleton instance;
+    public List<revisedBoid> boids = new List<revisedBoid>();
+    public GameObject birdy;
+    public Text cohesionRO;
+    public Text alignRO;
+    public Text seperationRO;
+    public Text birdCountRO;
+    public Text momMagRO;
+    public Text setMagRO;
+    public float cohesionDistance = 0;
+    public float alignDistance = 0;
+    public float seperationDistance = 0;
+    public float momMag = 0;
+    public float setMag = 0;
+    public List<Slider> UI_sliders;
+    public List<float> defaultSliderValues;
+    int targetBirdCount = 0;
+    int minFieldSize = 10;
+    int maxFieldSize = 30;
+    public int fieldSize = 0;
+
+    void changeFieldSize() {
+        fieldSize = targetBirdCount / 4;
+        if (fieldSize < minFieldSize)
+        {
+            fieldSize = minFieldSize;
+        }
+        else if (fieldSize > maxFieldSize) {
+            fieldSize = maxFieldSize;
+        }
+        gameObject.transform.position = new Vector3(0, 0, -(fieldSize + 5));
+        foreach (revisedBoid b in boids) {
+            b.fieldSize = fieldSize;
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Awake () {
+        if (singleton.instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else {
+            instance = this;
+        }
+	}
+    public void changeBirdCount(float value) {
+        int _value = Mathf.FloorToInt(value);
+
+        if (_value > boids.Count)
+        {
+            for (int i = 0; i < _value - boids.Count; i++)
+            {
+                Instantiate<GameObject>(birdy);
+            }
+        }
+        else if (boids.Count > _value) {
+            for (int i = 0; i < boids.Count - _value; i++) {
+                Destroy(boids[i].gameObject);
+                boids.RemoveAt(i);
+            }
+        }
+        targetBirdCount = _value;
+        birdCountRO.text = _value.ToString();
+        changeFieldSize();
+    }
+
+    public void changeMomMag(float value)
+    {
+        foreach (revisedBoid b in boids)
+        {
+            b.momMagnitude = value;
+        }
+        momMagRO.text = value.ToString();
+        momMag = value;
+    }
+
+    public void changeSetMag(float value)
+    {
+        foreach (revisedBoid b in boids)
+        {
+            b.settingMagnitudes = value;
+        }
+        setMagRO.text = value.ToString();
+        setMag = value;
+    }
+
+
+    public void changeCohesionDistance(float value) {
+        foreach (revisedBoid b in boids) {
+            b.cohesionDistance = value;
+        }
+        cohesionRO.text = value.ToString();
+        cohesionDistance = value;
+    }
+    public void changeSeperationDistance(float value)
+    {
+        foreach (revisedBoid b in boids)
+        {
+            b.avoidanceDistance = value;
+        }
+        seperationRO.text = value.ToString();
+        seperationDistance = value;
+    }
+    public void changeAlignDistance(float value)
+    {
+        foreach (revisedBoid b in boids)
+        {
+            b.alignDistance = value;
+        }
+        alignRO.text = value.ToString();
+        alignDistance = value;
+    }
+
+
+    private void Start()
+    {
+        for (int i = 0; i < UI_sliders.Count; i++) {
+            UI_sliders[i].value = defaultSliderValues[i];
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (targetBirdCount != boids.Count) {
+            changeBirdCount(targetBirdCount);
+        }
+	}
+}
